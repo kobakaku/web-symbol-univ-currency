e2479db8f96f49e9c1ec61ba5ece35bed79e09a5
kobakaku <kobakaku1616@gmail.com>
2023-05-19 22:58:51 +0900

    delete: integrate-to-web

--- projects/admin/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { APP_ROUTES } from './app/app.routes';
-import { provideRouter } from '@angular/router';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(APP_ROUTES)],
-});
